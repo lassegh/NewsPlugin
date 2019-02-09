ba4613e5c1ce9ee337f1770f66d8fcf0fb13e273
Lasse Grønbech <lasse.groenbech@gmail.com>
2019-02-09 15:15:52 +0100

    C

--- NewsPlugin/Feeds.cs
+++ NewsPlugin/Feeds.cs
@@ -8,13 +8,21 @@ namespace NewsPlugin
 {
     static class Feeds
     {
-        public static Dictionary<String,bool> FeedsDictionary = new Dictionary<string, bool>()
+        public static Dictionary<string,bool> FeedsDictionary = new Dictionary<string, bool>()
         {
             {"Dr1",true},
             {"Tv2",true },
             {"Bt",true },
             {"Eb",true }
         };
-        
+        //
+        public static Dictionary<string, string> FeedsUrl = new Dictionary<string, string>()
+        {
+            {"Dr1","https://www.dr.dk/nyheder/service/feeds/allenyheder"},
+            {"Tv2","http://feeds.tv2.dk/nyhederne_seneste/rss" },
+            {"Bt","https://www.bt.dk/bt/seneste/rss" },
+            {"Eb","https://ekstrabladet.dk/rssfeed/all/" }
+        };
+
     }
 }
