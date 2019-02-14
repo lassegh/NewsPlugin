8f5369d86760222d6d3c3a6dc92d0ca4e914801c
Kjergaard <KjergaardVFX@gmail.com>
2019-02-14 08:51:25 +0100

    implementeret settings checkboxe

--- NewsPlugin/Class1.cs
+++ NewsPlugin/Class1.cs
@@ -6,9 +6,13 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Forms;
 using System.Windows.Threading;
 using Wox.Infrastructure.Storage;
 using Wox.Plugin;
+using CheckBox = System.Windows.Controls.CheckBox;
+using Control = System.Windows.Controls.Control;
+using Label = System.Windows.Controls.Label;
 
 namespace NewsPlugin
 {
@@ -29,9 +33,63 @@ namespace NewsPlugin
                     var bw = new Window();
                     StackPanel stackPanel = new StackPanel();
                     Label label = new Label();
-                    label.Content = "Fuck";
+                    label.Content = "Settings";
+                    label.Height = 100;
+                    label.Width = 50;
+
+                    CheckBox dynamicCheckBox_DR = new CheckBox();
+                    dynamicCheckBox_DR.Name = "DynamicCheckBox_DR";
+                    dynamicCheckBox_DR.Content = "DR";
+                    dynamicCheckBox_DR.IsChecked = true;
+                    dynamicCheckBox_DR.Width = 50;
+                    dynamicCheckBox_DR.Height = 50;
+
+                    if (dynamicCheckBox_DR.IsChecked == false)
+                    {
+                        Feeds.FeedsDictionary["Dr1"] = false;
+                    }
+
+                    CheckBox dynamicCheckBox_TV2 = new CheckBox();
+                    dynamicCheckBox_TV2.Name = "DynamicCheckBox_TV2";
+                    dynamicCheckBox_TV2.Content = "TV2";
+                    dynamicCheckBox_TV2.IsChecked = true;
+                    dynamicCheckBox_TV2.Width = 50;
+                    dynamicCheckBox_TV2.Height = 50;
+
+                    if (dynamicCheckBox_TV2.IsChecked == false)
+                    {
+                        Feeds.FeedsDictionary["Tv2"] = false;
+                    }
+
+                    CheckBox dynamicCheckBox_BT = new CheckBox();
+                    dynamicCheckBox_BT.Name = "DynamicCheckBox_BT";
+                    dynamicCheckBox_BT.Content = "BT";
+                    dynamicCheckBox_BT.IsChecked = true;
+                    dynamicCheckBox_BT.Width = 50;
+                    dynamicCheckBox_BT.Height = 50;
+
+                    if (dynamicCheckBox_BT.IsChecked == false)
+                    {
+                        Feeds.FeedsDictionary["Bt"] = false;
+                    }
+
+                    CheckBox dynamicCheckBox_EB = new CheckBox();
+                    dynamicCheckBox_EB.Name = "DynamicCheckBox_EB";
+                    dynamicCheckBox_EB.Content = "Ekstra Bladet";
+                    dynamicCheckBox_EB.Width = 50;
+                    dynamicCheckBox_EB.Height = 50;
+
+                    if (dynamicCheckBox_EB.IsChecked == false)
+                    {
+                        Feeds.FeedsDictionary["Eb"] = false;
+                    }
+                    else Feeds.FeedsDictionary["Eb"] = true;
 
                     stackPanel.Children.Add(label);
+                    stackPanel.Children.Add(dynamicCheckBox_DR);
+                    stackPanel.Children.Add(dynamicCheckBox_TV2);
+                    stackPanel.Children.Add(dynamicCheckBox_BT);
+                    stackPanel.Children.Add(dynamicCheckBox_EB);
                     bw.Content = stackPanel;
                     bw.Show();
                     bw.Closed += (s, e) => bw.Dispatcher.InvokeShutdown();
@@ -43,7 +101,7 @@ namespace NewsPlugin
 
                 return true;// True bestemmer at wox skal lukke, når man trykker på story
             };
-
+            
             results.Add(newSetting); // tilføjer til listen
 
             if (query.RawQuery.Length > 4)
